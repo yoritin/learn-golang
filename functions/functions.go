2b818fe0ba0039b4061f151c3574930a56c86e0c
yoritin <47884591+yoritin@users.noreply.github.com>
2020-12-05 20:46:56 +0900

    first commit

---
+++ functions/functions.go
@@ -0,0 +1,5 @@
+package functions
+
+func Add(x int, y int) int {
+	return x + y
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "time"
+	"math/rand"
+	"./hello"
+	"./functions"
+)
+
+func main() {
+	randnum()
+	hello.Hello()
+	fmt.Println(functions.Add(34, 678))
+}
+
+func randnum() {
+  rand.Seed(time.Now().UnixNano())
+  fmt.Println("My favorite number is", rand.Intn(10))
+}
